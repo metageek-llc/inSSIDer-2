b7dcd12476af60402027b52ba96db8096e9abcec
Tyler Adkisson <tyleradkisson@gmail.com>
2010-10-18 16:16:53 -0400

    Tyler: Forgot to include SudoInterface in ManagedWiFi project. Updated version numbers to 2.0.5.1018 and rebuilt installers

--- ManagedWifi/AssemblyInfo.cs
+++ ManagedWifi/AssemblyInfo.cs
@@ -1,6 +1,6 @@
 ﻿// Assembly ManagedWifi, Version 2010.6.21.0
 
-[assembly: System.Reflection.AssemblyVersion("2010.10.15.0")]
+[assembly: System.Reflection.AssemblyVersion("2010.10.18.0")]
 [assembly: System.Reflection.AssemblyConfiguration("")]
 [assembly: System.Runtime.CompilerServices.CompilationRelaxations(8)]
 [assembly: System.Reflection.AssemblyTitle("Managed Wifi API")]
@@ -8,10 +8,10 @@
 [assembly: System.Runtime.CompilerServices.RuntimeCompatibility(WrapNonExceptionThrows=true)]
 [assembly: System.Reflection.AssemblyCompany("Monfort Software Engineering")]
 [assembly: System.Reflection.AssemblyProduct("Managed Wifi API")]
-[assembly: System.Reflection.AssemblyCopyright("Copyright \x00a9 Monfort Software Engineering 2007")]
+[assembly: System.Reflection.AssemblyCopyright("Copyright © Monfort Software Engineering 2007")]
 [assembly: System.Reflection.AssemblyTrademark("")]
 [assembly: System.Runtime.InteropServices.ComVisible(false)]
 [assembly: System.Runtime.InteropServices.Guid("e7646a9f-2d07-455a-b9ae-51eaf7281d2f")]
-[assembly: System.Reflection.AssemblyFileVersion("1.0.9.1015")]
+[assembly: System.Reflection.AssemblyFileVersion("1.0.10.1018")]
 [assembly: System.Diagnostics.Debuggable(System.Diagnostics.DebuggableAttribute.DebuggingModes.IgnoreSymbolStoreSequencePoints)]
 
--- MetaScanner/Properties/AssemblyInfo.cs
+++ MetaScanner/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.4.1015")]
-[assembly: AssemblyFileVersion("2.0.4.1015")]
+[assembly: AssemblyVersion("2.0.5.1018")]
+[assembly: AssemblyFileVersion("2.0.5.1018")]
