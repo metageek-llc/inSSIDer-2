886b3ac57da7c646c2831d003ab77b562e89348b
Tyler Adkisson <tyleradkisson@gmail.com>
2010-11-20 00:31:42 -0500

    Tyler: Started work on new tab control.

---
+++ MetaScanner/UI/Controls/TabControl.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace inSSIDer.UI.Controls
+{
+    partial class TabControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // TabControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ForeColor = System.Drawing.Color.Lime;
+            this.Name = "TabControl";
+            this.Size = new System.Drawing.Size(480, 360);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
---
+++ MetaScanner/UI/Forms/frmTest.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace inSSIDer.UI.Forms
+{
+    partial class frmTest
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new inSSIDer.UI.Controls.TabControl();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.tabControl1.BackColor = System.Drawing.Color.Black;
+            this.tabControl1.ForeColor = System.Drawing.Color.Lime;
+            this.tabControl1.Location = new System.Drawing.Point(13, 13);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.Size = new System.Drawing.Size(470, 397);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // frmTest
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(495, 422);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "frmTest";
+            this.Text = "frmTest";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private inSSIDer.UI.Controls.TabControl tabControl1;
+    }
+}
