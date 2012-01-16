fad2574e24e1226e6eb09ca46e5735c3631512cb
Ashok Gelal <ashok@metageek.net>
2012-01-16 12:39:29 -0700

    Added test project

---
+++ TestProject/ScanControllerTest.cs
@@ -0,0 +1,78 @@
+﻿using inSSIDer.Scanning;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace TestProject
+{
+    
+    
+    /// <summary>
+    ///This is a test class for ScanControllerTest and is intended
+    ///to contain all ScanControllerTest Unit Tests
+    ///</summary>
+    [TestClass()]
+    public class ScanControllerTest
+    {
+
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        // 
+        //You can use the following additional attributes as you write your tests:
+        //
+        //Use ClassInitialize to run code before running the first test in the class
+        //[ClassInitialize()]
+        //public static void MyClassInitialize(TestContext testContext)
+        //{
+        //}
+        //
+        //Use ClassCleanup to run code after all tests in a class have run
+        //[ClassCleanup()]
+        //public static void MyClassCleanup()
+        //{
+        //}
+        //
+        //Use TestInitialize to run code before running each test
+        //[TestInitialize()]
+        //public void MyTestInitialize()
+        //{
+        //}
+        //
+        //Use TestCleanup to run code after each test has run
+        //[TestCleanup()]
+        //public void MyTestCleanup()
+        //{
+        //}
+        //
+        #endregion
+
+
+        /// <summary>
+        ///A test for StartScanning
+        ///</summary>
+        [TestMethod()]
+        public void StartScanningTest()
+        {
+            ScanController target = new ScanController();
+            target.StartScanning();
+            Assert.IsNotNull(target);
+        }
+    }
+}
